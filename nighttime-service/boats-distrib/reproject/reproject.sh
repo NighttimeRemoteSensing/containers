85debfa3a8443dec9767971dd021e429d31acd18
tgregory <trifonov.grigory@gmail.com>
2017-04-03 11:25:55 +0300

    Piggyback on mcr java.

--- nighttime-service/boats-distrib/reproject/reproject.sh
+++ nighttime-service/boats-distrib/reproject/reproject.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-java -Xmx16484M -cp "$DIR/sis-jhdf5-batteries_included.jar:$DIR/jcoord-1.1-b.jar:$DIR/viirs_reprojection.jar" viirs.ViirsProcessor $1
+/opt/mcr/v81/sys/java/jre/glnxa64/jre/bin/java -Xmx16484M -cp "$DIR/sis-jhdf5-batteries_included.jar:$DIR/jcoord-1.1-b.jar:$DIR/viirs_reprojection.jar" viirs.ViirsProcessor $1
